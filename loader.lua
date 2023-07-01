a695c64a18a28d7dd014965c660d1534fd1d771a
ScriptForgeW <138209478+ScriptForgeW@users.noreply.github.com>
2023-07-01 14:21:02 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 if game.PlaceId == 13127800756 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptForgeW/ScriptForge/main/games/Arm%20Wrestle%20Simulator.lua"))()
-else if game.PlaceId == 6938803436 then
+elseif game.PlaceId == 6938803436 or game.PlaceId == 7274690025 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptForgeW/ScriptForge/main/games/Anime%20Dimensions.lua"))()
 else
     warn("No game found")
